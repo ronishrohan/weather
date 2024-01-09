680fe7d70bfc0a9bed9eb054f64a2a9ceb011863
Roni <64074505+RonishRohan@users.noreply.github.com>
2024-01-09 18:08:29 +0530

    added details

--- src/components/cards/Details.jsx
+++ src/components/cards/Details.jsx
@@ -2,8 +2,17 @@ import { motion } from "framer-motion";
 
 function Details(props) {
   let data = props.data;
+  function getTimeFromDate(dateStr){
+    let date = new Date(dateStr);
+    let hours = Math.abs(12 - date.getHours());
+    let minutes = date.getMinutes();
+    hours = hours<10? "0" + hours : hours;
+    minutes = minutes<10? "0" + minutes : minutes;
+    return(hours + ":" + minutes)
+  }
 
   return (
+    
     <>
       <motion.div
         initial={{ opacity: 0, y: -100 }}
@@ -19,28 +28,30 @@ function Details(props) {
         <div id="mrc-left">
           <div className="details-container">
             <div>Wind</div>
-            <div
-              id="wind-details"
-              className="details-text"
-            >{`${data.current.wind_speed_10m} m/s`}</div>
+            <div className="details-text">{`${data.current.wind_speed_10m} m/s`}</div>
           </div>
           <div className="details-container">
             <div>Humidity</div>
-            <div
-              id="humidity-details"
-              className="details-text"
-            >{`${data.current.relative_humidity_2m}%`}</div>
+            <div className="details-text">{`${data.current.relative_humidity_2m}%`}</div>
           </div>
           <div className="details-container">
             <div>Cloud cover</div>
-            <div
-              id="cloud-details"
-              className="details-text"
-            >{`${data.current.cloud_cover}%`}</div>
+            <div className="details-text">{`${data.current.cloud_cover}%`}</div>
           </div>
         </div>
         <div id="mrc-right">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          <div className="details-container">
+            <div>Pressure</div>
+            <div className="details-text">{`${data.current.pressure_msl} hPa`}</div>
+          </div>
+          <div className="details-container">
+            <div>Sunrise</div>
+            <div className="details-text">{`${getTimeFromDate(data.daily.sunrise[0])} am`}</div>
+          </div>
+          <div className="details-container">
+            <div>Sunset</div>
+            <div className="details-text">{`${getTimeFromDate(data.daily.sunset[0])} pm`}</div>
+          </div>
         </div>
       </motion.div>
     </>
