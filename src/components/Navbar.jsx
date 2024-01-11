3703bf65f197fe5047a4768c1f513ee0c24b2361
Roni <64074505+RonishRohan@users.noreply.github.com>
2024-01-11 22:21:23 +0530

    fixed alert message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,8 +11,8 @@ function Navbar(props) {
       <motion.div
           //initial={{ opacity: 0, x:20 }}
           //animate={{ opacity: 1, x:0 }}
-          initial={{scale: 0, opacity: 0, rotate: "360deg"}}
-          animate={{scale: 1, opacity: 1, rotate: 0}}
+          initial={{scale: 0, opacity: 0 }}
+          animate={{scale: 1, opacity: 1}}
           transition={{duration:0.5, delay: 0.6+delay, type: "tween", ease: "backOut", rotate:{
             duration: 1, delay: 0.8+delay, ease: "backInOut"
           }}}
